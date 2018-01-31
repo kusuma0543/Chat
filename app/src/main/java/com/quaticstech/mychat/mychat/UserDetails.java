eaedde744abd42acd3af9692422c27cbb10ae008
andriod <kusumachowdary0543@gmail.com>
2018-01-31 17:34:58 +0530

    Initial commit

---
+++ app/src/main/java/com/quaticstech/mychat/mychat/UserDetails.java
@@ -0,0 +1,10 @@
+package com.quaticstech.mychat.mychat;
+
+/**
+ * Created by andriod on 31/1/18.
+ */
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
